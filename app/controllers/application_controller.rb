4ddba6e80b42c6c88aa2a9c7d14696e90914530b
rsundar <rohan.s.1987@gmail.com>
2019-12-12 00:48:32 +0530

    finished excercise 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     def hello
-        render html: "hello, world!"
+        render html: "¡hola, mundo!"
     end
+
+    def goodbye
+        render html: "Well, then goodbye!"
+    end
+
 end
